5d81b685ea205eb694ceb0a26bfc631bdd20a112
Eliot BOUTHERIN <ebouther@e2r5p3.42.fr>
2015-12-20 18:50:37 +0100

    works with stupid AI

--- resolution.c
+++ resolution.c
@@ -6,7 +6,7 @@
 /*   By: ebouther <ebouther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/20 16:34:26 by ebouther          #+#    #+#             */
-/*   Updated: 2015/12/20 18:37:30 by ebouther         ###   ########.fr       */
+/*   Updated: 2015/12/20 18:49:46 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 static void	ft_del_first_node(t_list **lst)
 {
-	t_list tmp;
+	t_list *tmp;
 	if (lst == NULL)
 		return ;
 	if (*lst)
@@ -37,29 +37,51 @@ static int ft_ia(t_list **lst)
 	int	xor;
 
 	xor = 0;
-	begin = *lst;
-	while (*lst)
+	begin = *lst;	
+	ft_putendl("IA's TURN");
+	((t_board *)((*lst)->content))->matches -= 1; // CHANGE 1
+	if (((t_board *)((*lst)->content))->matches == 0)
+	{
+		if ((*lst)->next != NULL)
+			ft_del_first_node(lst);
+		else
+		{
+			ft_putendl("YOU WIN !");
+			return (0);
+		}
+	}
+	/*while (*lst)
 	{
 		printf("matches '%d'\n", ((t_board *)(*lst)->content)->matches);
 		xor = xor ^ ((t_board *)(*lst)->content)->matches;
 		*lst = (*lst)->next;
-	}
-	printf("xor : '%d'\n", xor);
+	}*/
+	//printf("xor : '%d'\n", xor);
+	//
 	return (0);
 }
 
-static int ft_player(t_list **lst)i
+static int ft_player(t_list **lst)
 {
 	char	*str;
 	int		nb;
 
+	ft_putendl("PLAYER's TURN");
 	get_next_line(0, &str);
 	nb = ft_atoi(str);
 	((t_board *)((*lst)->content))->matches -= nb;
 	if (((t_board *)((*lst)->content))->matches < 0)
 		ft_player(lst);
 	if (((t_board *)((*lst)->content))->matches == 0)
-		ft_del_first_node(lst);
+	{
+		if ((*lst)->next != NULL)
+			ft_del_first_node(lst);
+		else
+		{
+			ft_putendl("YOU LOSE !");
+			return (0);
+		}
+	}
 	return (0);
 }
 
