fd381d6a35edbf81d79e96379be807757a0527e8
Eliot BOUTHERIN <ebouther@e2r5p3.42.fr>
2016-01-30 22:17:29 +0100

    update

--- 2048/utils.c
+++ 2048/utils.c
@@ -6,13 +6,49 @@
 /*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/30 21:15:31 by ebouther          #+#    #+#             */
-/*   Updated: 2016/01/30 21:19:46 by ebouther         ###   ########.fr       */
+/*   Updated: 2016/01/30 22:11:58 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
 void		ft_print_map(t_env *e)
+{
+	int		i;
+	int		y;
+	int		n;
+	int		max_y;
+	int		max_x;
+
+	max_x = 0;
+	max_y = 0;
+	getmaxyx(stdscr, max_y, max_x);
+	clear();
+	border('|', '|', '-', '_', '*', '*', '*', '*');
+	i = 1;
+	while (i < BOARD)
+	{
+		mvhline((max_y / BOARD) * i, 0, '-', max_x);
+		mvvline(0, (max_x / BOARD) * i, '|', max_y);
+		i++;
+	}
+	y = 1;
+	n = 0;
+	while (y < BOARD * 2)
+	{
+		i = 1;
+		while (i < BOARD * 2)
+		{
+			mvprintw((max_y / (BOARD * 2)) * y, (max_x / (BOARD * 2)) * i, "%d", e->map[n++]);
+			i += 2;
+		}
+		y += 2;
+	}
+	i = 0;
+	refresh();
+}
+
+void		ft_print_map_dbg(t_env *e)
 {
 	int	i;
 	int	x;
